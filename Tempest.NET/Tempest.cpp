6e71ecdb5af8a61f1dbbf74f389545213fc26ca2
notjulie <rasmussenrandy189@gmail.com>
2015-04-29 06:26:29 -0500

    Made the initial connection of TempestWpf all the way down to TempestLib.

--- Tempest.NET/Tempest.cpp
+++ Tempest.NET/Tempest.cpp
@@ -1,17 +1,34 @@
 
 
 #include "stdafx.h"
+
+#include "TempestLib/CPU6502.h"
+#include "TempestLib/TempestBus.h"
+#include "TempestLib/Win32/Win32PerformanceCounter3KHzClock.h"
+
 #include "Tempest.h"
 
+
+using namespace System;
 
 namespace TempestDotNET {
 	Tempest::Tempest(void)
 	{
+		clock = new Win32PerformanceCounter3KHzClock();
+		tempestBus = new TempestBus(clock);
+		cpu6502 = new CPU6502(tempestBus);
+	}
+
+	Tempest::~Tempest(void)
+	{
+		delete cpu6502, cpu6502 = NULL;
+		delete tempestBus, tempestBus = NULL;
+		delete clock, clock = NULL;
 	}
 
 	void Tempest::Start(void)
 	{
-
+		throw gcnew NotImplementedException("Tempest::Start");
 	}
 }
 
--- Tempest.NET/Tempest.h
+++ Tempest.NET/Tempest.h
@@ -2,13 +2,24 @@
 #ifndef TEMPEST_H
 #define TEMPEST_H
 
+class CPU6502;
+class TempestBus;
+class Win32PerformanceCounter3KHzClock;
+
 namespace TempestDotNET {
 
 	public ref class Tempest
 	{
 	public:
 		Tempest();
+		~Tempest();
+
 		void Start(void);
+
+	private:
+		CPU6502		*cpu6502;
+		TempestBus	*tempestBus;
+		Win32PerformanceCounter3KHzClock *clock;
 	};
 
 }
