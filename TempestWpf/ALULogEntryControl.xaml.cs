902f3fa44599322d6de1715b591a5025e33a0a7a
notjulie <rasmussenrandy189@gmail.com>
2015-05-03 18:20:52 -0500

    Added user control for the ALU log entries.

---
+++ TempestWpf/ALULogEntryControl.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TempestWpf
+{
+   /// <summary>
+   /// Interaction logic for ALULogEntryControl.xaml
+   /// </summary>
+   public partial class ALULogEntryControl : UserControl
+   {
+      /// <summary>
+      /// Initializes a new instance of class ALULogEntryControl
+      /// </summary>
+      public ALULogEntryControl()
+      {
+         InitializeComponent();
+      }
+   }
+}
