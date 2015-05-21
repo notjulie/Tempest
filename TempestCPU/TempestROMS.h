23ddbcbd75a102431b09be753af3eecee352f8fb
notjulie <rasmussenrandy189@gmail.com>
2015-05-20 23:45:46 -0500

    Added C array versions of the ROMs.

---
+++ TempestCPU/TempestROMS.h
@@ -0,0 +1,27 @@
+
+#ifndef TEMPESTROMS_H
+#define TEMPESTROMS_H
+
+
+const uint8_t ROM_136002_111[2048];
+const uint8_t ROM_136002_112[2048];
+const uint8_t ROM_136002_113[2048];
+const uint8_t ROM_136002_114[2048];
+const uint8_t ROM_136002_115[2048];
+const uint8_t ROM_136002_116[2048];
+const uint8_t ROM_136002_117[2048];
+const uint8_t ROM_136002_118[2048];
+const uint8_t ROM_136002_119[2048];
+const uint8_t ROM_136002_120[2048];
+const uint8_t ROM_136002_121[2048];
+const uint8_t ROM_136002_122[2048];
+const uint8_t ROM_136002_126[32];
+const uint8_t ROM_136002_127[256];
+const uint8_t ROM_136002_128[256];
+const uint8_t ROM_136002_129[256];
+const uint8_t ROM_136002_130[256];
+const uint8_t ROM_136002_131[256];
+const uint8_t ROM_136002_132[256];
+
+
+#endif
