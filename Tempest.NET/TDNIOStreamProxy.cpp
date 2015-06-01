f57b92c0a2cade63cd4165c066a5a4cc28855ba7
notjulie <rasmussenrandy189@gmail.com>
2015-06-01 17:11:21 -0500

    Initial add of TempestDisco project

---
+++ Tempest.NET/TDNIOStreamProxy.cpp
@@ -0,0 +1,26 @@
+
+#include "Stdafx.h"
+
+#include "TempestIO/TempestIOStreamProxy.h"
+#include "TempestIO/TempestMemoryStream.h"
+
+#include "TDNIOStreamProxy.h"
+
+namespace TempestDotNET {
+
+	TDNIOStreamProxy::TDNIOStreamProxy(TDNMemoryStream ^stream)
+	{
+		// keep a managed reference to the stream so that it doesn't get
+		// garbage collected while we're using it
+		managedStream = stream;
+
+		// create our native proxy object
+		tempestIO = new TempestIOStreamProxy(managedStream->GetStream());
+	}
+
+
+	TDNIOStreamProxy::~TDNIOStreamProxy(void)
+	{
+		delete tempestIO, tempestIO = NULL;
+	}
+};
---
+++ TempestIO/TempestStreamProtocol.h
@@ -0,0 +1,12 @@
+
+#ifndef TEMPESTSTREAMPROTOCOL_H
+#define TEMPESTSTREAMPROTOCOL_H
+
+enum StreamOpCodes
+{
+	OP_VECTOR_RESET = 0,
+	OP_6KHZ_TICK = 1,
+	OP_WRITE_VECTOR_RAM = 2
+};
+
+#endif
