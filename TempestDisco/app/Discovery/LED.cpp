bddabacba47cffe915195b57bafa3f11c18b0762
notjulie <rasmussenrandy189@gmail.com>
2015-07-25 11:08:39 -0500

    Got rid of the hacked version of stm32f4_discovery.h and am finally using the correct version.

--- TempestDisco/app/Discovery/LED.cpp
+++ TempestDisco/app/Discovery/LED.cpp
@@ -2,6 +2,23 @@
 #include "TempestDisco.h"
 #include "LED.h"
 
+#define LED_GREEN_PIN                    GPIO_Pin_12
+#define LED_GREEN_GPIO_PORT              GPIOD
+#define LED_GREEN_GPIO_CLK               RCC_AHB1Periph_GPIOD
+
+#define LED_ORANGE_PIN                   GPIO_Pin_13
+#define LED_ORANGE_GPIO_PORT             GPIOD
+#define LED_ORANGE_GPIO_CLK              RCC_AHB1Periph_GPIOD
+
+#define LED_RED_PIN                      GPIO_Pin_14
+#define LED_RED_GPIO_PORT                GPIOD
+#define LED_RED_GPIO_CLK                 RCC_AHB1Periph_GPIOD
+
+#define LED_BLUE_PIN                     GPIO_Pin_15
+#define LED_BLUE_GPIO_PORT               GPIOD
+#define LED_BLUE_GPIO_CLK                RCC_AHB1Periph_GPIOD
+
+
 void InitializeLEDs(void)
 {
 	// enable the peripheral clock for GPIOD
