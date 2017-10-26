c89cf8aca39ba461d80d356b144dc04dfa1a0a75
notjulie <rasmussenrandy189@gmail.com>
2017-10-25 20:43:57 -0500

    Changed SQLRRException to use ostringstream instead of sprintf... trying to get out of the habit of the unsafe functions.

--- SQLite/SQLRR/SQLRRException.h
+++ SQLite/SQLRR/SQLRRException.h
@@ -6,41 +6,11 @@
 
 class SQLRRException : public TempestException {
 public:
-   SQLRRException(sqlite3 *db, int error, const std::string &message)
-      : TempestException(FormatMessage(db, error, message))
-   {
-   }
+   SQLRRException(sqlite3 *db, int error, const std::string &message);
 
 private:
-   static std::string FormatMessage(sqlite3 *db, int error, const std::string &message)
-   {
-      std::string result("SQLite exception");
-
-      char s[100];
-      snprintf(s, sizeof(s), ", Error #%d", error);
-      result += s;
-
-      // add the error code
-      switch (error)
-      {
-      case SQLITE_CANTOPEN:
-         result += " (SQLITE_CANTOPEN: ";
-         result += GetExtendedErrorString(db);
-         result += ")";
-         break;
-      }
-
-      return result;
-   }
-
-   static std::string GetExtendedErrorString(sqlite3 *db)
-   {
-      int extendedError = sqlite3_extended_errcode(db);
-
-      char s[100];
-      snprintf(s, sizeof(s), "0x%04X", extendedError);
-      return s;
-   }
+   static std::string FormatMessage(sqlite3 *db, int error, const std::string &message);
+   static std::string GetExtendedErrorString(sqlite3 *db);
 };
 
 
--- SQLite/SQLiteCommonHeader.h
+++ SQLite/SQLiteCommonHeader.h
@@ -1,3 +1,4 @@
 
 #include "sqlite3.h"
+#include <sstream>
 #include <string>
