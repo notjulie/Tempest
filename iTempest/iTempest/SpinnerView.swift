e96acd50aace7358930f5d30a9f4cabab249d263
notjulie <rasmussenrandy189@gmail.com>
2018-01-20 07:37:33 -0600

    Got rid of SpinnerLayer in favor of inheriting SpinnerView from MTKView.

--- iTempest/iTempest/SpinnerView.swift
+++ iTempest/iTempest/SpinnerView.swift
@@ -8,69 +8,152 @@
 
 import Foundation
 import UIKit
+import MetalKit
 
-class SpinnerView : UIView
+class SpinnerView : MTKView
 {
-    private enum TouchState {
-        case NoTouch
-        case Dragging
-        case Coasting
-    };
-    
-    private var touchState : TouchState = TouchState.NoTouch;
-    
-    private var tempest : cTempest = 0;
-    private var initialized : Bool = false;
-    private var encoderChange: Int32 = 0;
+   private enum TouchState {
+      case NoTouch
+      case Dragging
+      case Coasting
+   };
+   
+   // tempest
+   private var initialized : Bool = false;
+   private var tempest : cTempest = 0;
 
-    private var timer : Timer? = nil;
+   // metal related properties
+   private var commandQueue: MTLCommandQueue?
+   private var renderPipelineState: MTLRenderPipelineState?
+   private var depthStencilState: MTLDepthStencilState?
+   private var vertexBuffer: MTLBuffer! = nil;
+   private var renderParametersBuffer: MTLBuffer! = nil;
+   private var rotation : Float = 0;
+   
+   // swiping related
+   private var touchState : TouchState = TouchState.NoTouch;
+   private var encoderChange: Int32 = 0;
+   private var timer : Timer? = nil;
+   private var startX : CGFloat = 0;
+   private var lastX : CGFloat = 0;
+   private var lastTouchTime : CGFloat = 0;
+   private var speed : CGFloat = 0;
+   private var coastDistance : CGFloat = 0;
     
-    private var startX : CGFloat = 0;
-    private var lastX : CGFloat = 0;
-    private var lastTouchTime : CGFloat = 0;
-    private var speed : CGFloat = 0;
-    private var coastDistance : CGFloat = 0;
-    
-    // the number of encoder ticks to move per pixel
-    private let ticksPerPixel : CGFloat = 1.0;
+   // the number of encoder ticks to move per pixel
+   private let ticksPerPixel : CGFloat = 1.0;
 
-    // Maximum encoder ticks per second while coasting... if this gets too high
-    // we'll end up with encoder wrap-around
-    private let maxCoastSpeed : CGFloat = 1000;
+   // Maximum encoder ticks per second while coasting... if this gets too high
+   // we'll end up with encoder wrap-around
+   private let maxCoastSpeed : CGFloat = 1000;
     
-    // time (in seconds) of the speed lowpass; this is just in place to make sure that
-    // one sample alone doesn't define our coast speed, such as if the touchscreen
-    // says the user slowed down when in fact he just eased off the screen
-    private let speedLowpassTime = 0.2;
+   // time (in seconds) of the speed lowpass; this is just in place to make sure that
+   // one sample alone doesn't define our coast speed, such as if the touchscreen
+   // says the user slowed down when in fact he just eased off the screen
+   private let speedLowpassTime = 0.2;
     
-    private let minCoastSpeed : CGFloat = 0;
-    private let coastDeceleration : CGFloat = 200;
-    
-    private let speedLog = NSMutableArray();
+   private let minCoastSpeed : CGFloat = 0;
+   private let coastDeceleration : CGFloat = 200;
+   
+   private let speedLog = NSMutableArray();
 
-    func setTempest(tempest:cTempest) {
-        if (!initialized) {
-            backgroundColor = UIColor.blue;
-            isUserInteractionEnabled = true;
+   
+   func setTempest(tempest:cTempest) {
+      if (!initialized) {
+         backgroundColor = UIColor.blue;
+         isUserInteractionEnabled = true;
             
-            timer = Timer.scheduledTimer(
+         timer = Timer.scheduledTimer(
                 timeInterval: 0.01,
                 target: self,
                 selector: #selector(swipeTimer),
                 userInfo: nil,
                 repeats: true);
-            
-            // add our SpinnerLayer to show the spinner
-            let spinnerLayer = SpinnerLayer();
-            spinnerLayer.initialize();
-            self.layer.addSublayer(spinnerLayer);
-            
-            initialized = true;
-        }
-        self.tempest = tempest;
-    }
+         
+         initializeMetal()
+         
+         initialized = true;
+      }
+      self.tempest = tempest;
+   }
+   
+   
+   private func initializeMetal() {
+      // Device
+      device = MTLCreateSystemDefaultDevice()
+      
+      // View
+      clearColor = MTLClearColorMake(0.0, 0.0, 0.0, 1)
+      colorPixelFormat = .bgra8Unorm
+      depthStencilPixelFormat = .depth32Float
+      
+      // Command queue
+      commandQueue = device!.makeCommandQueue()
+      
+      // Render pipeline
+      let library = device!.newDefaultLibrary()!
+      let vertexFunction = library.makeFunction(name: "basic_vertex")
+      let fragmentFunction = library.makeFunction(name: "basic_fragment")
+      let renderPipelineDescriptor = MTLRenderPipelineDescriptor()
+      renderPipelineDescriptor.vertexFunction = vertexFunction
+      renderPipelineDescriptor.fragmentFunction = fragmentFunction
+      renderPipelineDescriptor.colorAttachments[0].pixelFormat = colorPixelFormat
+      renderPipelineDescriptor.depthAttachmentPixelFormat = depthStencilPixelFormat
+      do {
+         renderPipelineState = try device!.makeRenderPipelineState(descriptor: renderPipelineDescriptor)
+      } catch {
+         Swift.print("Unable to compile render pipeline state")
+         return
+      }
+      
+      // Depth stencil
+      let depthSencilDescriptor = MTLDepthStencilDescriptor()
+      depthSencilDescriptor.depthCompareFunction = .less
+      depthSencilDescriptor.isDepthWriteEnabled = true
+      depthStencilState = device!.makeDepthStencilState(descriptor: depthSencilDescriptor)
+      
+      // create our vertex data... this never changes, we just update the rotation
+      var vertexData : [SpinnerVertex] = [
+         SpinnerVertex(position:0, corner:0),
+         SpinnerVertex(position:0, corner:1),
+         SpinnerVertex(position:0, corner:2)
+      ];
+      let dataSize = vertexData.count * MemoryLayout.size(ofValue: vertexData[0]);
+      vertexBuffer = device!.makeBuffer(bytes: vertexData, length: dataSize, options: []);
+   }
 
-    func swipeTimer() {
+   override func draw(_ dirtyRect: CGRect) {
+      
+      let commandBuffer = commandQueue!.makeCommandBuffer()
+      let renderPassDescriptor = currentRenderPassDescriptor!
+      let renderEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: renderPassDescriptor)
+      
+      renderEncoder.setFrontFacing(.counterClockwise)
+      renderEncoder.setDepthStencilState(depthStencilState)
+      renderEncoder.setRenderPipelineState(renderPipelineState!)
+      
+      // set our render parameters
+      rotation += 6;
+      if (rotation >= 360)
+      {
+         rotation -= 360;
+      }
+      let renderParameters : SpinnerRenderParameters =
+         SpinnerRenderParameters(rotation: self.rotation);
+      let dataSize = MemoryLayout.size(ofValue: renderParameters);
+      renderParametersBuffer = device!.makeBuffer(bytes: [renderParameters], length: dataSize, options: []);
+      
+      renderEncoder.setVertexBuffer(vertexBuffer, offset: 0, at: Int(SPINNER_VERTICES_BUFFER));
+      renderEncoder.setVertexBuffer(renderParametersBuffer, offset: 0, at: Int(SPINNER_RENDER_PARAMETERS_BUFFER));
+      renderEncoder.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: 3, instanceCount: 1)
+      
+      renderEncoder.endEncoding()
+      let drawable = currentDrawable!
+      commandBuffer.present(drawable)
+      commandBuffer.commit()
+   }
+
+   func swipeTimer() {
         if (touchState != TouchState.Coasting)
         {
             return;
