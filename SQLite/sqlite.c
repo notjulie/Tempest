854e73d461a2cd7541aef26c58bd76c2b724da49
notjulie <rasmussenrandy189@gmail.com>
2018-03-31 19:25:10 -0500

    Cleaned up SQLite so that it doesn't issue so many warnings in XCode.

---
+++ SQLite/sqlite.c
@@ -0,0 +1,26 @@
+//
+//  sqlite.c
+//  iTempest
+//
+//  Created by Randy Rasmussen on 3/31/18.
+//  Copyright © 2018 Randy Rasmussen. All rights reserved.
+//
+
+// MIN and MAX get multiply defined and the compiler doesn't seem to honor the
+// prepreocessor directives that prevent it.  sqlite.c has its own definitions
+// for them, so make sure include files that we need to include and then undefine
+// MIN and MAX.
+#include <sys/param.h>
+
+// undefine things that sqlite3 has its own definitions for
+#undef MIN
+#undef MAX
+
+// SQLite uses the comma operator a lot... suppress warnings about it
+#pragma GCC diagnostic ignored "-Wcomma"
+
+// and it's not perfectly anal about its integer conversions
+#pragma GCC diagnostic ignored "-Wconversion"
+
+// include sqlite
+#include "sqlite3.c"
