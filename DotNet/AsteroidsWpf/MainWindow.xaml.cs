247893a7326add3df535af7aec1b18057ec7f8a1
notjulie <rasmussenrandy189@gmail.com>
2018-05-12 12:23:58 -0500

    Changed some Tempest names in AsteroidsWpf.

--- DotNet/AsteroidsWpf/MainWindow.xaml.cs
+++ DotNet/AsteroidsWpf/MainWindow.xaml.cs
@@ -2,22 +2,15 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
 
 using TempestDotNET;
 
-namespace TempestWpf
+namespace AsteroidsWpf
 {
    /// <summary>
    /// Interaction logic for MainWindow.xaml
