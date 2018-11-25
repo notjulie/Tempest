c02cfb1bc5f5e67bcb444b04e499d88c5177b2ee
notjulie <rasmussenrandy189@gmail.com>
2018-11-25 13:13:38 -0600

    Documentation break.

--- DotNet/TempestWpf/App.xaml.cs
+++ DotNet/TempestWpf/App.xaml.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿// ====================================================================
+// Tempest WPF version
+//    Author: Randy Rasmussen
+//    Copyright: none... do what you will
+//    Warranties: none... do what you will at your own risk
+//
+// File summary:
+//    This is the main application for TempestWPF, which is mainly
+//    a testbed for my Tempest code so I can 1) have a nice comfy
+//    place for testing it and 2) enforce platform independence.
+// ====================================================================
+
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
--- TempestDisco/app/stm32f4xx_it.c
+++ TempestDisco/app/stm32f4xx_it.c
@@ -1,5 +1,14 @@
-#include "stm32f4xx_it.h"
-#include "stm32f4xx_conf.h"
+// ====================================================================
+// stm32f4xx_it.c
+//    Author: Randy Rasmussen
+//    Copyright: none... do what you will
+//    Warranties: none... do what you will at your own risk
+//
+// File summary:
+//    Contains the implementations of some of the standard ARM
+//    interrupt handlers, such as bus fault and the like.
+// ====================================================================
+
 
 #include "SystemError.h"
 
@@ -80,75 +89,3 @@ void PendSV_Handler(void)
 {
 }
 
-/******************************************************************************/
-/*                 STM32F4xx Peripherals Interrupt Handlers                   */
-/*  Add here the Interrupt Handler for the used peripheral(s) (PPP), for the  */
-/*  available peripheral interrupt handler's name please refer to the startup */
-/*  file (startup_stm32f4xx.s).                                               */
-/******************************************************************************/
-
-#ifdef USE_USB_OTG_FS
-void OTG_FS_WKUP_IRQHandler(void)
-{
-  if(USB_OTG_dev.cfg.low_power)
-  {
-    *(uint32_t *)(0xE000ED10) &= 0xFFFFFFF9 ;
-    SystemInit();
-    USB_OTG_UngateClock(&USB_OTG_dev);
-  }
-  EXTI_ClearITPendingBit(EXTI_Line18);
-}
-#endif
-
-/**
-  * @brief  This function handles EXTI15_10_IRQ Handler.
-  * @param  None
-  * @retval None
-  */
-#ifdef USE_USB_OTG_HS
-void OTG_HS_WKUP_IRQHandler(void)
-{
-  if(USB_OTG_dev.cfg.low_power)
-  {
-    *(uint32_t *)(0xE000ED10) &= 0xFFFFFFF9 ;
-    SystemInit();
-    USB_OTG_UngateClock(&USB_OTG_dev);
-  }
-  EXTI_ClearITPendingBit(EXTI_Line20);
-}
-#endif
-
-/**
-  * @brief  This function handles OTG_HS Handler.
-  * @param  None
-  * @retval None
-  */
-#ifdef USE_USB_OTG_HS
-void OTG_HS_IRQHandler(void)
-#else
-void OTG_FS_IRQHandler(void)
-#endif
-{
-}
-
-#ifdef USB_OTG_HS_DEDICATED_EP1_ENABLED
-/**
-  * @brief  This function handles EP1_IN Handler.
-  * @param  None
-  * @retval None
-  */
-void OTG_HS_EP1_IN_IRQHandler(void)
-{
-  USBD_OTG_EP1IN_ISR_Handler (&USB_OTG_dev);
-}
-
-/**
-  * @brief  This function handles EP1_OUT Handler.
-  * @param  None
-  * @retval None
-  */
-void OTG_HS_EP1_OUT_IRQHandler(void)
-{
-  USBD_OTG_EP1OUT_ISR_Handler (&USB_OTG_dev);
-}
-#endif
