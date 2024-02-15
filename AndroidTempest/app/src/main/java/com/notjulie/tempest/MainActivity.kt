14ef4d934a9718e2a5399b9d76e2819a50e95be2
Randy Rasmussen <randy@evergreenresearch.com>
2024-02-14 19:27:55 -0600

    Resolved some AndroidStudio complaints about my Kotlin.

--- AndroidTempest/app/src/main/java/com/notjulie/tempest/MainActivity.kt
+++ AndroidTempest/app/src/main/java/com/notjulie/tempest/MainActivity.kt
@@ -22,7 +22,6 @@ class MainActivity : AppCompatActivity(), SurfaceHolder.Callback {
     private lateinit var handler : Handler
     private var currentHolder: SurfaceHolder? = null
     private var location: Float = 0.0F
-    private val background : Paint = Paint()
     private val tempest : Tempest = Tempest()
 
     @RequiresApi(Build.VERSION_CODES.P)
@@ -36,7 +35,7 @@ class MainActivity : AppCompatActivity(), SurfaceHolder.Callback {
         //binding.sampleText.text = stringFromJNI()
 
         // get our view
-        view = findViewById(R.id.surfaceView);
+        view = findViewById(R.id.surfaceView)
         view.background = ColorDrawable()
         val surface : SurfaceHolder = view.holder
         surface.addCallback(this)
@@ -47,27 +46,27 @@ class MainActivity : AppCompatActivity(), SurfaceHolder.Callback {
         // schedule something to do
         Timer().scheduleAtFixedRate(object : TimerTask() {
             override fun run() {
-                location = location + 10
+                location += 10
                 val holder: SurfaceHolder? = currentHolder
                 if (holder == null)
                     return
                 val canvas : Canvas = holder.lockHardwareCanvas()
 
-                val backgroumd = Paint()
-                backgroumd.setAntiAlias(true)
-                backgroumd.setDither(true)
-                backgroumd.setColor(0x1FFFF00)
-                backgroumd.setStyle(Paint.Style.FILL)
+                val background = Paint()
+                background.isAntiAlias = true
+                background.isDither = true
+                background.color = 0x1FFFF00
+                background.style = Paint.Style.FILL
                 canvas.drawRect(0.0F, 0.0F, canvas.width.toFloat(), canvas.height.toFloat(), background)
 
                 val paint = Paint()
-                paint.setAntiAlias(true)
-                paint.setDither(true)
-                paint.setColor(-0x100FF00)
-                paint.setStyle(Paint.Style.STROKE)
-                paint.setStrokeJoin(Paint.Join.ROUND)
-                paint.setStrokeCap(Paint.Cap.ROUND)
-                paint.setStrokeWidth(10.0F)
+                paint.isAntiAlias = true
+                paint.isDither = true
+                paint.color = -0x100FF00
+                paint.style = Paint.Style.STROKE
+                paint.strokeJoin = Paint.Join.ROUND
+                paint.strokeCap = Paint.Cap.ROUND
+                paint.strokeWidth = 10.0F
 
                 canvas.drawOval(location,location,100.0F,100.0F, paint)
                 holder.unlockCanvasAndPost(canvas)
@@ -101,13 +100,13 @@ class MainActivity : AppCompatActivity(), SurfaceHolder.Callback {
     override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
         val canvas : Canvas = holder.lockHardwareCanvas()
         val paint = Paint()
-        paint.setAntiAlias(true)
-        paint.setDither(true)
-        paint.setColor(-0x100FF00)
-        paint.setStyle(Paint.Style.STROKE)
-        paint.setStrokeJoin(Paint.Join.ROUND)
-        paint.setStrokeCap(Paint.Cap.ROUND)
-        paint.setStrokeWidth(10.0F)
+        paint.isAntiAlias = true
+        paint.isDither = true
+        paint.color = -0x100FF00
+        paint.style = Paint.Style.STROKE
+        paint.strokeJoin = Paint.Join.ROUND
+        paint.strokeCap = Paint.Cap.ROUND
+        paint.strokeWidth = 10.0F
 
         canvas.drawOval(0.0F,0.0F,100.0F,100.0F, paint)
         holder.unlockCanvasAndPost(canvas)
